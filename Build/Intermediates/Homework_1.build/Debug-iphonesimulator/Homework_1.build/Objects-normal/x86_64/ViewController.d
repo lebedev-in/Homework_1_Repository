ff1ce58e114d9e338b1e6722c8d6f09aef425900
Ilya Lebedev <estrenuo@yandex.ru>
2017-01-31 13:59:53 +0300

    Image1 was added.

---
+++ Build/Intermediates/Homework_1.build/Debug-iphonesimulator/Homework_1.build/Objects-normal/x86_64/ViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/ila_lebedev/Documents/Программирование/Курс\ "Программист\ iOS"/Objective\ C.\ Уровень\ 2/Урок\ 1/Homework_1/Homework_1/ViewController.m \
+  /Users/ila_lebedev/Documents/Программирование/Курс\ "Программист\ iOS"/Objective\ C.\ Уровень\ 2/Урок\ 1/Homework_1/Homework_1/ViewController.h
---
+++ Build/Intermediates/Homework_1.build/Debug-iphonesimulator/Homework_1.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/ila_lebedev/Documents/Программирование/Курс\ "Программист\ iOS"/Objective\ C.\ Уровень\ 2/Урок\ 1/Homework_1/Homework_1/main.m \
+  /Users/ila_lebedev/Documents/Программирование/Курс\ "Программист\ iOS"/Objective\ C.\ Уровень\ 2/Урок\ 1/Homework_1/Homework_1/AppDelegate.h
